8504b908b622d2e1e6034a0ca560f726d7b6bd43
j.s <joananeves03@gmail.com>
2024-05-15 16:14:28 +0100

    last version

--- breast_lesion_DL_pack/api/fast.py
+++ breast_lesion_DL_pack/api/fast.py
@@ -124,4 +124,13 @@ def predict(preprocessed_image):
 
     # Predict with the model
     result = model.predict(img)
-    return result
+
+    benign_prob = round(result[0][0], 2)
+    malignant_prob = round(result[0][1], 2)
+
+    if benign_prob >= 0.7:
+        return f'The predicted probability of the lesion being benign is {benign_prob}'
+    elif malignant_prob >= 0.7:
+        return f'The predicted probability of the lesion malignant benign is {malignant_prob}'
+    else:
+        return f'Uncertainty warning! The predicted probability of the lesion being benign is only: {benign_prob}, and being malignant: {malignant_prob}'
